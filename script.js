aa2336097572cb106fcd4e72b7da9713bad02171
tree462214 <87408369+tree462214@users.noreply.github.com>
2025-09-15 23:02:37 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const video = document.getElementById('video');
+const audio = document.getElementById('audio');
+const button = document.getElementById('button');
+
+button.addEventListener('mousedown', startPress);
+button.addEventListener('touchstart', startPress);
+
+button.addEventListener('mouseup', endPress);
+button.addEventListener('mouseleave', endPress);
+button.addEventListener('touchend', endPress);
+button.addEventListener('touchcancel', endPress);
+
+function startPress() {
+
+    video.loop = true;
+    audio.loop = true;
+
+    audio.volume = 0.2; 
+
+    audio.play();
+    video.play();
+}
+
+function endPress() {
+    audio.pause();
+    video.pause();
+
+    video.loop = false;
+    audio.loop = false;
+}
+
+
+
+
+
+
